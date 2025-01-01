5b1914a3a95d796c60b725ec794c7f15a7edbfea
MINJE JO <jo@MINJEs-MacBook-Air.local>
2025-01-01 13:42:30 +0900

    from 3.5

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,14 @@
-export default function Home() {
-  return (
-    <main className="bg-gray-300 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-2">In transit</span>
-            <span className="text-4xl font-semibold">Coolblue</span>
-          </div>
-          <div className="size-12 bg-orange-400 rounded-full"/>
+  export default function Home() {
+    return (
+      <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
+        <div className="bg-white w-full shadow-lg p-5 rounded-3xl before:max-w-screen-sm flex flex-col gap-3">
+          <input className="w-full rounded-full bg-gray-200 pl-5 h-12 ring ring-orange-400 ring-offset-2" 
+          type="text" 
+          placeholder="Search here..."/>
+          <button className="bg-black bg-opacity-50 text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none">Search</button>
         </div>
-        <div>
-          <span>Today</span>
-          <span>09:30 -12:00</span>
-        </div>
-        <div>
-          <div />
-          <div />
-        </div>
-        <div>
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span>Delivered</span>
-        </div>
-      </div>
 
-    </main>
-  );
-}
+      </main>
+    );
+  }
 
