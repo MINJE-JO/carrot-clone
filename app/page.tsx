b4c1e3f6c18779bc2efc3f9698143db86bf43fc8
MINJE JO <jo@MINJEs-MacBook-Air.local>
2025-01-10 09:58:59 +0900

    3.8 1:40

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
-  export default function Home() {
+export default function Home() {
     return (
-      <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
-        <div className="bg-white w-full shadow-lg p-5 rounded-3xl before:max-w-screen-sm flex flex-col gap-3">
-          <input className="w-full rounded-full bg-gray-200 pl-5 h-12 ring ring-orange-400 ring-offset-2" 
-          type="text" 
-          placeholder="Search here..."/>
-          <button className="bg-black bg-opacity-50 text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none">Search</button>
-        </div>
-
-      </main>
+        <main
+            className="bg-gray-100 h-screen sm:bg-red-500 flex items-center justify-center p-5 ">
+            <div
+                className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col">
+                {["nico","me","you"].map((person,index) =>(
+                    <div key={index}>
+                        <span>{person}</span>
+                        <div>
+                            <span>{index}</span>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </main>
     );
-  }
-
+}
