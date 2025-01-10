ceec03af336c217d336484a2e8537af85b31ce69
MINJE JO <jo@MINJEs-MacBook-Air.local>
2025-01-10 23:16:50 +0900

    link hover global부터

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,20 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <span>당근마켓</span>
-        <h1>당근</h1>
-        <h2>당근마켓에 어서오세요</h2>
+    <div className="flex flex-col  items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-2xl">당근임티</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근마켓에 어서오세요</h2>
       </div>
-      <div>
-        <Link href="/create-account">Create Account</Link>
-        <div>
+      <div className="flex flex-col items-center gap-2 w-full">
+        <Link
+          href="/create-account"
+          className="w-full bg-orange-500 text-white text-lg font-medium p-2 rounded-md text-center hover:bg-orange-600 transition-colors"
+        >
+          Create Account
+        </Link>
+        <div className="flex gap-2">
           <span>이미 계정이 있나요?</span>
           <Link href="/login">로그인</Link>
         </div>
